037a16a416a85a90d94d6445d7ec884c06899f63
Yves Tanas <Yves.Tanas@gmx.de>
2026-03-03 11:04:11 +0100

    Create __init__.py

---
+++ features/__init__.py
@@ -0,0 +1,7 @@
+"""
+Features package: website generation, plots, and other feature modules.
+"""
+from features import movies_website
+from features import movies_plots
+
+__all__ = ["movies_website", "movies_plots"]
